ec0495f020aaec285aacedb714fa099163fe4e15
talharradhika2507 <talharradhika2507@gmail.com>
2026-01-25 17:14:07 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountID=123456
+let accountEmail="Radha@gmail.com"
+var accountPassword="121212"
+accountCity="Amaravti"
+
+//accountID=11111
+
+let = accountEmail="aa@gmail.com"
+accountPassword="141414"
+accountCity="Nashik"
+accontstate=
+
+console.log(accountEmail);
+
+//console.table([accountEmail,accountPassword,accountCity,accontstate]);
+
+
