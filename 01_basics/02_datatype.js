561f996d797aa9474826c4baa409a333d686b827
talharradhika2507 <talharradhika2507@gmail.com>
2026-01-26 17:14:05 +0000

    Configuration done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,25 @@
+
+// console.log(typeof undefined)
+// console.log(typeof null)
+
+symbol=123
+//symbol="Radhika"
+console.log(symbol)
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//symbol => unique
+
+
+//object
+
+//let num = 567
+// console.log(num)
+// console.log(typeof num)
+
+let num = true
+console.log(num)
+console.log(typeof num)
---
+++ 01_basics/03_conversionofDatatype.js
@@ -0,0 +1,25 @@
+
+// let num = "1123"
+// let convertedNumber = Number(num)
+// console.log(convertedNumber)
+// console.log(typeof convertedNumber)
+
+// let num = 0825
+// let convertedString = String(num)
+// console.log(convertedString)
+// console.log(typeof convertedString)
+
+// let num = 0
+// let convertedBoolean = Boolean(num)
+// console.log(convertedBoolean)
+// console.log(typeof convertedBoolean)
+
+// let num = false
+// let convertedNumber = Number(num)
+// console.log(convertedNumber)
+// console.log(typeof convertedNumber)
+
+*******************************************OPERATION***********************************************
+
+
+
