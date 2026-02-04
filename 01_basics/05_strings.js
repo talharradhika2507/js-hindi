de9f607f77445d086330c49219bffb0396f1681f
talharradhika2507 <talharradhika2507@gmail.com>
2026-02-04 14:19:43 +0000

    container config done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,39 @@
+const Name = "Radhika_hide_and_sick"
+// const repoCount = 50
+
+//console.log(Name + repoCount + " value");
+
+//console.log(`My Name is ${Name} and my repocount is ${repoCount}`);
+
+// const gameName = new String("RadhikaAG")
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(8));
+// console.log(gameName.indexOf("R"));
+
+//const Name = new String("Anchanesh")
+
+const NewString = Name.substring(0, 5)
+console.log(NewString);
+
+const anotherString = Name.slice(-8, 5)
+console.log(anotherString);
+
+const stringOne = "     Anchu     "
+console.log(stringOne);
+console.log(stringOne.trim());
+
+const url = "https://anchuradha//Radhika/Anchanesh%20SweetHome"
+
+console.log(url.replace("%20", "_"));
+console.log(url.includes("Anchanesh"));
+console.log(url.includes("    "));
+
+console.log(Name.split("_"));
+
+
+ 
+
