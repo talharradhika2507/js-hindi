0254bd45470963e1a7ee7a42d0573bf68df5a03c
talharradhika2507 <talharradhika2507@gmail.com>
2026-02-09 12:44:22 +0000

    Math and Num

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,45 @@
+
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(2));
+
+// const newNumb = 12.8765
+// console.log(newNumb.toPrecision(4));
+
+// const hundred = 1000000000000000000
+// console.log(hundred.toLocaleString('en-LN'));
+
+//+++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,5,6,7,));
+// console.log(Math.max(4,5,6,7,));
+// console.log(Math.round(4.6));
+
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()*10 + 1));
+
+const min= 10
+const max= 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+
+
+
+
+
